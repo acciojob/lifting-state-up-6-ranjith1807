52361f40100e833b09c0cd021813582761ab9f56
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-05-04 21:57:10 +0530

    Refactor TodoList component for improved readability

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,23 +3,17 @@ import React from 'react';
 function TodoList({ todos, handleComplete }) {
   return (
     <div>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} data-testid={`todo-${todo.id}`}>
-            {todo.text}
-
-            {/* Button only appears if NOT completed */}
-            {!todo.completed && (
-              <button
-                data-testid={`complete-${todo.id}`}
-                onClick={() => handleComplete(todo.id)}
-              >
-                Complete
-              </button>
-            )}
-          </li>
-        ))}
-      </ul>
+    <h2 style={{paddingLeft:'40px'}}>Child Component</h2>
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => handleComplete(todo.id)}>
+            Complete
+          </button>
+        </li>
+      ))}
+    </ul>
     </div>
   );
 }
