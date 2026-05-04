fac8df4101a8ae80eabee6516cbffe4e83cfa6de
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-05-04 21:42:38 +0530

    Refactor App component and update heading

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,15 @@ import React, { useState } from 'react';
 import TodoList from './TodoList';
 
 function App() {
-  // Initialize state with an array of objects
   const [todos, setTodos] = useState([
     { id: 1, text: 'Learn React', completed: false },
     { id: 2, text: 'Build a React app', completed: false },
     { id: 3, text: 'Deploy the React app', completed: false }
   ]);
 
-  // Function to update completion state
- const handleComplete = (id) => {
-    // We MUST return a NEW array via .map() so React detects the change
-    setTodos(prevTodos => 
-      prevTodos.map(todo => 
+  const handleComplete = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
         todo.id === id ? { ...todo, completed: true } : todo
       )
     );
@@ -21,8 +18,7 @@ function App() {
 
   return (
     <div>
-      <h1>Parent Component</h1>
-      {/* Pass todos and handleComplete as props */}
+      <h1>Todo App</h1>
       <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
   );
