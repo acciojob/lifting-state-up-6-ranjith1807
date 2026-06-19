5bdb9c9270b6ba0fccbfea02707d23b2ae112eed
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-06-19 11:10:53 +0530

    mark all todos completed on any complete click

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: true },
+    { id: 1, text: 'Learn React', completed: false },
     { id: 2, text: 'Build a React app', completed: false },
-    { id: 3, text: 'Deploy the React app', completed: true },
+    { id: 3, text: 'Deploy the React app', completed: false },
   ]);
 
   const handleComplete = (id) => {
     setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id ? { ...todo, completed: true } : todo
-      )
+      prevTodos.map((todo) => ({ ...todo, completed: true }))
     );
   };
 
