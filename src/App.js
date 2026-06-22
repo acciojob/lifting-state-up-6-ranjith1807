8d5398d92d1fea50a9833d2f690923939379753c
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-06-22 19:06:54 +0530

    efwrt5yuytgyjhj

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import TodoList from "./TodoList";
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, text: "Learn React", completed: false },
-    { id: 2, text: "Build Todo App", completed: false },
-    { id: 3, text: "Practice State", completed: false }
+    { id: 1, text: "Learn React", completed: false }
   ]);
 
   const handleComplete = (id) => {
-    setTodos(
-      todos.map((todo) =>
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
         todo.id === id ? { ...todo, completed: true } : todo
       )
     );
